a79cdef698067f9da09de03d159892ce28b39690
vthuyan <132875152+vthuyan@users.noreply.github.com>
2023-07-17 20:51:34 +0700

    Create edit-car.php

---
+++ product/edit-car.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'pdo.php';
+require_once 'helper.php';
+
+$request = $_POST;
+
+ $name = $request['name'];
+ $id = $request['id'];
+ $price = $request['price'];
+ $ca_id = $request['ca_id'];
+
+// update($name, $price, $ca_id, $id);
+$getinf = new Query();
+$products = $getinf->update($name, $price, $ca_id, $id);
+redirectHome();
