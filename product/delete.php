5240744abfd7a808cb7ee857f64528134e42dabe
vthuyan <132875152+vthuyan@users.noreply.github.com>
2023-07-17 20:51:30 +0700

    Create delete.php

---
+++ product/delete.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'pdo.php';
+require_once 'helper.php';
+
+if ($_POST['id'] > 0 && is_numeric($_POST['id'])) {
+    $getinf = new Query();
+    $products = $getinf->delete(['id' => $_POST['id']]);
+    // delete(['id' => $_POST['id']]);
+}
+
+redirectHome();
